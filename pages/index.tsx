1341c96f385e72618eec31deabac91cef1c19504
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-11-09 20:25:32 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
 import type { NextPage } from "next"
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
-import NavBar from "../components/NavBar.tsx"
-import Disconnected from '../components/Disconnected.tsx'
+import NavBar from "../components/NavBar"
+import Disconnected from '../components/Disconnected'
 import { useWallet } from "@solana/wallet-adapter-react"
-import Connected from "../components/Connected.tsx"
+import Connected from "../components/Connected"
 
 const Home: NextPage = () => {
   const { connected } = useWallet()
