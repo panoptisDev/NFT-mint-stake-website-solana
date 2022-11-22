e596d5c32de8e5dd731c158b376644ddb8c6b840
kudarukuni <krukuni@gmail.com>
2022-11-22 10:37:26 +0200

    i won the battle

--- tokens/bld/index.ts
+++ tokens/bld/index.ts
@@ -15,77 +15,27 @@ async function createBldToken(
   connection: web3.Connection,
   payer: web3.Keypair
 ) {
-    // This will create a token with all the necessary inputs
     const tokenMint = await token.createMint(
-        connection, // Connection
-        payer, // Payer
-        payer.publicKey, // Your wallet public key
-        payer.publicKey, // Freeze authority
-        2 // Decimals
+    connection,
+    payer,
+    payer.publicKey,
+    payer.publicKey,
+    2
     );
-
-    // Create a metaplex object so that we can create a metaplex metadata
-    const metaplex = Metaplex.make(connection)
-        .use(keypairIdentity(payer))
-        .use(
-        bundlrStorage({
-            address: "https://devnet.bundlr.network",
-            providerUrl: "https://api.devnet.solana.com",
-            timeout: 60000,
-        })
-        );
-
-    // Read image file
+    const metaplex = Metaplex.make(connection).use(keypairIdentity(payer)).use(bundlrStorage({ address: "https://devnet.bundlr.network", providerUrl: "https://api.devnet.solana.com", timeout: 60000, }));
     const imageBuffer = fs.readFileSync(TOKEN_IMAGE_PATH);
     const file = toMetaplexFile(imageBuffer, TOKEN_IMAGE_NAME);
     const imageUri = await metaplex.storage().upload(file);
-
-    // Upload the rest of offchain metadata
-    const { uri } = await metaplex
-        .nfts()
-        .uploadMetadata({
-        name: TOKEN_NAME,
-        description: TOKEN_DESCRIPTION,
-        image: imageUri,
-        })
-
-    // Finding out the address where the metadata is stored
+    const { uri } = await metaplex.nfts().uploadMetadata({ name: TOKEN_NAME, description: TOKEN_DESCRIPTION, image: imageUri, })
     const metadataPda = findMetadataPda(tokenMint);
-    const tokenMetadata = {
-        name: TOKEN_NAME,
-        symbol: TOKEN_SYMBOL,
-        uri: uri,
-        sellerFeeBasisPoints: 0,
-        creators: null,
-        collection: null,
-        uses: null,
-    } as DataV2
-
-    const instruction = createCreateMetadataAccountV2Instruction({
-        metadata: metadataPda,
-        mint: tokenMint,
-        mintAuthority: payer.publicKey,
-        payer: payer.publicKey,
-        updateAuthority: payer.publicKey
-    },
-    {
-        createMetadataAccountArgsV2: {
-            data: tokenMetadata,
-            isMutable: true
-        }
-    })
-
+    const tokenMetadata = { name: TOKEN_NAME, symbol: TOKEN_SYMBOL, uri: uri, sellerFeeBasisPoints: 0, creators: null, collection: null, uses: null, } as DataV2
+    const instruction = createCreateMetadataAccountV2Instruction({ metadata: metadataPda, mint: tokenMint, mintAuthority: payer.publicKey, payer: payer.publicKey, updateAuthority: payer.publicKey }, { createMetadataAccountArgsV2: { data: tokenMetadata, isMutable: true }})
     const transaction = new web3.Transaction()
     transaction.add(instruction)
 
-    const transactionSignature = await web3.sendAndConfirmTransaction(
-        connection,
-        transaction,
-        [payer]
-    )
+    const transactionSignature = await web3.sendAndConfirmTransaction(connection, transaction, [payer])
 }
 
-// The rest of your main function
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
   const payer = initializeKeypair;
