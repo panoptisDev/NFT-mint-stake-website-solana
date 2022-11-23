be7a44803c034df56e8cf50b3cdc4dd62c4f1f10
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-11-23 23:00:43 +0200

    Update Connected.tsx

--- components/Connected.tsx
+++ components/Connected.tsx
@@ -1,8 +1,28 @@
-import { Button, Container, Heading, VStack, Text, HStack, Image, } from "@chakra-ui/react"
-import { FC, MouseEventHandler, useCallback, useEffect, useMemo, useState, } from "react"
+import {
+  Button,
+  Container,
+  Heading,
+  VStack,
+  Text,
+  HStack,
+  Image,
+} from "@chakra-ui/react"
+import {
+  FC,
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react"
 import { PublicKey } from "@solana/web3.js"
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
-import { Metaplex, walletAdapterIdentity, CandyMachine, CandyMachineV2 } from "@metaplex-foundation/js"
+import {
+  Metaplex,
+  walletAdapterIdentity,
+  CandyMachine,
+  CandyMachineV2
+} from "@metaplex-foundation/js"
 import { useRouter } from "next/router"
 
 const Connected: FC = () => {
@@ -11,18 +31,72 @@ const Connected: FC = () => {
   const [candyMachine, setCandyMachine] = useState<CandyMachineV2>()
   const [isMinting, setIsMinting] = useState(false)
 
-  const metaplex = useMemo(() => { return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter))}, [connection, walletAdapter])
+  const metaplex = useMemo(() => {
+    return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter))
+  }, [connection, walletAdapter])
 
-  useEffect(() => { if (!metaplex) return metaplex.candyMachinesV2().findByAddress({ address: new PublicKey("H25KWpvYk752uCiXfm9ckLGcvAZNYeq6B9BCuok7dgoB"), }).then((candyMachine) => { console.log(candyMachine)setCandyMachine(candyMachine) }).catch((error) => { alert(error) }) }, [metaplex]);
+  useEffect(() => {
+    if (!metaplex) return
+
+    metaplex
+      .candyMachinesV2()
+      .findByAddress({
+        address: new PublicKey("H25KWpvYk752uCiXfm9ckLGcvAZNYeq6B9BCuok7dgoB"),
+      })
+      .then((candyMachine) => {
+        console.log(candyMachine)
+        setCandyMachine(candyMachine)
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }, [metaplex])
+
   const router = useRouter()
-  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(async (event) => { if (event.defaultPrevented) return if (!walletAdapter.connected || !candyMachine) { return } try { setIsMinting(true) const nft = await metaplex.candyMachinesV2().mint({ candyMachine, }); console.log(nft) router.push(`/newMint?mint=${nft.nft.address.toBase58()}`) } catch (error) { alert(error) } finally { setIsMinting(false) } }, [walletAdapter.connected, candyMachine, metaplex, router])
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    async (event) => {
+      if (event.defaultPrevented) return
+
+      if (!walletAdapter.connected || !candyMachine) {
+        return
+      }
+
+      try {
+        setIsMinting(true)
+        const nft = await metaplex.candyMachinesV2().mint({
+          candyMachine,
+    });
+        console.log(nft)
+        router.push(`/newMint?mint=${nft.nft.address.toBase58()}`)
+      } catch (error) {
+        alert(error)
+      } finally {
+        setIsMinting(false)
+      }
+    },
+    [walletAdapter.connected, candyMachine, metaplex, router]
+  )
 
   return (
     <VStack spacing={20}>
       <Container>
         <VStack spacing={8}>
-          <Heading color="white" as="h1" size="2xl" noOfLines={1} textAlign="center"> Welcome My Wellow Willow.</Heading>
-          <Text color="bodyText" fontSize="xl" textAlign="center">Each Willow is randomly generated and can be staked to receive<Text as="b"> WLW</Text>. Use your <Text as="b"> WLW</Text> to upgrade your Willow and receive perks within the community!</Text>
+          <Heading
+            color="white"
+            as="h1"
+            size="2xl"
+            noOfLines={1}
+            textAlign="center"
+          >
+            Welcome Buildoor.
+          </Heading>
+
+          <Text color="bodyText" fontSize="xl" textAlign="center">
+            Each buildoor is randomly generated and can be staked to receive
+            <Text as="b"> $BLD</Text>. Use your <Text as="b"> $BLD</Text> to
+            upgrade your buildoor and receive perks within the community!
+          </Text>
         </VStack>
       </Container>
 
@@ -34,8 +108,14 @@ const Connected: FC = () => {
         <Image src="avatar5.png" alt="" />
       </HStack>
 
-      <Button bgColor="accent" color="white" maxW="380px" onClick={handleClick} isLoading={isMinting}>
-        <Text>Mint Willow</Text>
+      <Button
+        bgColor="accent"
+        color="white"
+        maxW="380px"
+        onClick={handleClick}
+        isLoading={isMinting}
+      >
+        <Text>mint buildoor</Text>
       </Button>
     </VStack>
   )
