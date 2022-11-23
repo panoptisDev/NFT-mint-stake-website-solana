282c2075f94f9786e48d5863b0b079b10700fb4b
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-11-23 22:56:34 +0200

    Update newMint.tsx

--- pages/newMint.tsx
+++ pages/newMint.tsx
@@ -1,69 +1,101 @@
-import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
 import type { NextPage } from "next"
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
-import Disconnected from "../components/Disconnected"
-import NavBar from "../components/NavBar"
-import { useWallet } from "@solana/wallet-adapter-react"
-import Connected from "../components/Connected"
-import { Button, Text, HStack } from "@chakra-ui/react";
-import { MouseEventHandler, useCallback } from "react";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
-import { PublicKey } from "@solana/web3.js";
-import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import MainLayout from "../components/MainLayout"
+import {
+  Container,
+  Heading,
+  VStack,
+  Text,
+  Image,
+  Button,
+  HStack,
+} from "@chakra-ui/react"
+import {
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react"
+import { ArrowForwardIcon } from "@chakra-ui/icons"
+import { PublicKey } from "@solana/web3.js"
+import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js"
 
-interface NewMintProps {
-  mint: PublicKey;
-}
-
-const NewMint: NextPage = () => {
-  const [metadata, setMetadat] = useState<any>()
+const NewMint: NextPage<NewMintProps> = ({ mint }) => {
+  const [metadata, setMetadata] = useState<any>()
   const { connection } = useConnection()
   const walletAdapter = useWallet()
-  const metaplex = useMemo(() => { return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter)) }, [connection, walletAdapter]) const { connected } = useWallet() const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(async (event) => {}, []);
-  useEffect(() => { metaplex.nfts().findByMint({ mintAddress: mint }).run().then((nft) => { fetch(nft.uri).then((res) => res.json()).then((metadata) => { setMetadata(metadata) }) }) }, [mint, metaplex, walletAdapter])
-  
+  const metaplex = useMemo(() => {
+    return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter))
+  }, [connection, walletAdapter])
+
+  useEffect(() => {
+    metaplex
+      .nfts()
+      .findByMint({ mintAddress: mint })
+      .then((nft) => {
+        fetch(nft.uri)
+          .then((res) => res.json())
+          .then((metadata) => {
+            setMetadata(metadata)
+          })
+      })
+  }, [mint, metaplex, walletAdapter])
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    async (event) => {},
+    []
+  )
+
   return (
     <MainLayout>
-    <div className={styles.container}>
-      <Head><title>Willows</title><meta name="The NFT Cowllection for Willows" /><link rel="icon" href="/favicon.ico" /></Head>
+      <VStack spacing={20}>
+        <Container>
+          <VStack spacing={8}>
+            <Heading color="white" as="h1" size="2xl" textAlign="center">
+              😮 A new buildoor has appeared!
+            </Heading>
 
-      <Box w="full" h="calc(100vh)" bgImage={connected ? "" : "url(/home-background.svg)"} backgroundPosition="center">
-        <Stack w="full" h="calc(100vh)" justify="center">
-          <NavBar />
-          <Spacer />
-          <Center>{connected ? <Connected /> : <Disconnected />}</Center>
-          <Spacer />
-          <Center>
-            <Box marginBottom={4} color="white">
-              <a href="https://twitter.com/_buildspace" target="_blank" rel="noopener noreferrer">
-                Built By @Naswillow
-              </a>
-            </Box>
-          </Center>
-        </Stack>
-      </Box>
-    </div>
-    <Image src={metadata?.image ?? ""} alt="" /><Button bgColor="accent" color="white" maxWidth="380px" onClick={handleClick}>
-    <HStack>
-      <Text>Stake My Willow</Text>
-        <ArrowForwardIcon />
-    </HStack>
-    </Button>
+            <Text color="bodyText" fontSize="xl" textAlign="center">
+              Congratulations, you minted a lvl 1 buildoor! <br />
+              Time to stake your character to earn rewards and level up.
+            </Text>
+          </VStack>
+        </Container>
+
+        <Image src={metadata?.image ?? ""} alt="" />
+
+        <Button
+          bgColor="accent"
+          color="white"
+          maxW="380px"
+          onClick={handleClick}
+        >
+          <HStack>
+            <Text>stake my buildoor</Text>
+            <ArrowForwardIcon />
+          </HStack>
+        </Button>
+      </VStack>
     </MainLayout>
-  );
-};
+  )
+}
+
+interface NewMintProps {
+  mint: PublicKey
+}
 
 NewMint.getInitialProps = async ({ query }) => {
-  const { mint } = query;
-  if (!mint) throw { error: "No mint" };
+  const { mint } = query
+
+  if (!mint) throw { error: "no mint" }
 
   try {
-    const mintPubkey = new PublicKey(mint);
-    return { mint: mintPubkey };
+    const mintPubkey = new PublicKey(mint)
+    return { mint: mintPubkey }
   } catch {
-    throws({ error: "Invalid mint" });
+    throw { error: "invalid mint" }
   }
-};
+}
 
 export default NewMint
